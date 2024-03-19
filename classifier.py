ee61bc7d91be806c39d6bf21ef0a1767ee7d07ed
Manojsai787 <140172728+FlareMozZ@users.noreply.github.com>
2024-03-19 12:35:13 +0530

    location api

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, notify,navigate
+from httpx import Client
 from tensorflow.keras import models
 from PIL import Image
 import numpy as np
@@ -50,6 +51,8 @@ Upload an image of a plant
 
 """
 
+
+
 def on_change(state, var_name, var_val):
     if var_name == "content":
         state.pred = ""
@@ -59,8 +62,91 @@ def on_change(state, var_name, var_val):
             result_index = model_prediction(var_val)
             state.pred = f"The model predicted the plant to be {class_names[result_index]}"
     # print(var_name, var_val)
+            
 
-app = Gui(page=index)
+
+def latitude(place):
+    with Client() as client:
+        response = client.get(
+            url="https://api-test.openepi.io/geocoding/",
+            params={"q": place},
+        )
+
+        data = response.json()
+
+        # prints the coordinates of the first result
+        return data["features"][0]["geometry"]["coordinates"][1]
+    
+def longitude(place):
+    with Client() as client:
+        response = client.get(
+            url="https://api-test.openepi.io/geocoding/",
+            params={"q": place},
+        )
+
+        data = response.json()
+
+        # prints the coordinates of the first result
+        return data["features"][0]["geometry"]["coordinates"][0]
+
+def country(place):
+    with Client() as client:
+        response = client.get(
+            url="https://api-test.openepi.io/geocoding/",
+            params={"q": place},
+        )
+
+        data = response.json()
+
+        # prints the coordinates of the first result
+        return data["features"][0]["properties"]["country"]
+
+text = ""
+lat = ""
+long = ""
+cntry = ""
+
+index1 = """
+# Geocoding Service
+
+Place name: <|{text}|>
+
+<|{text}|input|>
+
+<|Get Coordinates|button|on_action=on_button_action|> <br/>
+Latitude is: <|{lat}|> <br/>
+Longitude is: <|{long}|> <br/>
+country: <|{cntry}|> <br/>
+
+"""
+
+def on_button_action(state):
+        state.lat = latitude(state.text)
+        state.long = longitude(state.text)
+        state.cntry = country(state.text)
+        notify(state, 'info', f'The place is: {state.text}')
+        notify(state, 'info', f'The country is: {state.cntry}')
+        notify(state, 'info', f'The latitude is: {state.lat}')
+        notify(state, 'info', f'The longitude is: {state.long}')
+        state.text = ""
+
+
+
+root_md="<|menu|label=Menu|lov={[('plantdisease', 'Check crop health'), ('soilinfo', 'Get soil info')]}|on_action=on_menu|>"
+
+
+def on_menu(state, action, info):
+    page = info["args"][0]
+    navigate(state, to=page)
+
+
+pages = {
+    "/": root_md,
+    "plantdisease": index,
+    "soilinfo": index1,
+}
+
+app = Gui(pages=pages)
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
